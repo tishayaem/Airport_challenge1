c3965d98ff6784a55702d7b735125e16489ead18
Constantin <kostiantynkalinin@gmail.com>
2016-02-06 23:26:49 +0000

    Tests for weather and plane classes

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,15 +1,17 @@
 require 'weather'
 
-describe Weather do
-  subject(:weather) {described_class.new}
-  describe '#stormy' do
-    it 'can be not stormy' do
+describe WeatherReporter do
+  subject(:weather) { described_class.new }
+
+  describe '#stormy?' do
+    it 'can be non-stormy' do
+      allow(Kernel).to receive(:rand).and_return 1
       expect(weather.stormy?).to be false
     end
-    it 'can be stormy' do
-      expect(weather.stormy?).to be true
 
+    it 'can be stormy' do
+      allow(Kernel).to receive(:rand).and_return 6
+      expect(weather.stormy?).to be true
     end
   end
-
 end
